a3fbbfc377fbf1cfbd7dcc33d5ede89199156a29
najiemayo <153330693+najiemayo@users.noreply.github.com>
2024-06-25 10:04:43 -0500

    initial upload

---
+++ data_analysis/VarCall/BRCA2mave24.ldaER.R
@@ -0,0 +1,28 @@
+########################################
+##  2024 Analysis of BRCA2 MAVE Data  ##
+##  Last Modified:   04/18/24 by ESI  ##
+########################################
+
+
+library(knitr)
+knit("BRCA2mave24.ldaER.Rtex")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
